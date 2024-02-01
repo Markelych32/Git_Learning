888405eca23dd1cff36355f0c93d080da1a7f37c
Mark Solonchev <solonchev32@gmail.com>
2024-02-01 23:19:33 +0300

    Map.iml added FirstGit.java added WriterRunner.java modified (changed)

--- src/stream/WriterRunner.java
+++ src/stream/WriterRunner.java
@@ -9,11 +9,5 @@ public class WriterRunner {
     public static void main(String[] args) throws IOException {
         Path file = Path.of("resources", "writer2.txt");
         Files.write(file, List.of("Hello, World!", "Java", "Python"));
-        //try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(file, true))) {
-//        try (BufferedWriter fileWriter = Files.newBufferedWriter(file)) {
-//            fileWriter.append("Hello, World!");
-//            fileWriter.newLine();
-//            fileWriter.append("Java");
-//        }
     }
 }
