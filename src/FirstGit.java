b64d970addaadf2fccb4a8baa787f5b73e98f42b
Mark Solonchev <solonchev32@gmail.com>
2024-02-02 01:14:08 +0300

    delete sout about difficult

--- src/FirstGit.java
+++ src/FirstGit.java
@@ -2,7 +2,6 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("Git Develop");
         System.out.println("Git learning!");
-        System.out.println("It's terribly difficult");
         System.out.println("I'a happy");
         System.out.println("Hello from Developer!!!");
     }
